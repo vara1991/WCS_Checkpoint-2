0d0687fc42a9db5981750ee8f7a52da0456d4726
aloz-lab <alozier45@gmail.com>
2020-05-07 10:03:13 +0200

    algorithm exercise

--- src/Service/Yodalizer.php
+++ src/Service/Yodalizer.php
@@ -7,10 +7,14 @@ namespace App\Service;
  */
 class Yodalizer
 {
-
     public static function yodalizeIt(string $str):string
     {
         //TODO Write your code here,
         //TODO And return what we are waiting for at the end...
+        $explode = explode(' ', $str);
+        $reverse = array_reverse($explode);
+        $string = implode(' ', $reverse);
+        return ucfirst($string);
     }
+
 }
