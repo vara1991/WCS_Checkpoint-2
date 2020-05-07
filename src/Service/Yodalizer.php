5480a70c85d85cd9efe95622e3fdf0fcd755d0b1
JennyViannay <jenny.viannay75@gmail.com>
2020-05-07 11:57:39 +0200

    comment algo

--- src/Service/Yodalizer.php
+++ src/Service/Yodalizer.php
@@ -10,19 +10,24 @@ class Yodalizer
 
     public static function yodalizeIt(string $str):string
     {
-        // transforme la strin en tableau 
+        // transforme la string en array 
         $xplode = explode(' ', $str);
         // inverse array
         $reverse = array_reverse($xplode);
+        // prepare new array of result 
         $array = [];
+        // loop on reverse
         for($i = 0; $i < count($reverse); $i++){
+            // for i = 0 (first word) always put majuscule && push in it on new array
             if($i == 0){
                 array_push($array, ucfirst($reverse[$i]));
                 
             } else {
+                // for other i, always push in new array
                 array_push($array, $reverse[$i]);
             }
         }
+        // finaly use implode to get string with values of new array and return result of it
         return implode(' ', $array); 
     }
 }
