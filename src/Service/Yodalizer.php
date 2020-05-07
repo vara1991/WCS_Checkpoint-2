8ea2d745c24d73828e878ab285ae1707f6dcc772
Lisa-Mhl <michellisa.75@gmail.com>
2020-05-07 12:00:03 +0200

    Yodalizer ok but check failed

--- src/Service/Yodalizer.php
+++ src/Service/Yodalizer.php
@@ -8,9 +8,16 @@ namespace App\Service;
 class Yodalizer
 {
 
-    public static function yodalizeIt(string $str):string
+    public static function yodalizeIt(string $str): string
     {
         //TODO Write your code here,
+        $string = "I feel fear here";
+
+        $explode = explode(" ", $string);
+        $reverse = array_reverse($explode);
+        $implode = implode(" ", $reverse);
+
         //TODO And return what we are waiting for at the end...
+        return ucfirst($implode);
     }
 }
