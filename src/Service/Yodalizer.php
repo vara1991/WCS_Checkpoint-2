4955dd66407a67322e36db4a7cd4893b5228e5da
JennyViannay <jenny.viannay75@gmail.com>
2020-05-07 15:56:05 +0200

    fix

--- src/Service/Yodalizer.php
+++ src/Service/Yodalizer.php
@@ -11,23 +11,14 @@ class Yodalizer
     public static function yodalizeIt(string $str):string
     {
         // transforme la string en array 
-        $xplode = explode(' ', $str);
+        //$expl = explode(' ', $str);
         // inverse array
-        $reverse = array_reverse($xplode);
-        // prepare new array of result 
-        $array = [];
-        // loop on reverse
-        for($i = 0; $i < count($reverse); $i++){
-            // for i = 0 (first word) always put majuscule && push in it on new array
-            if($i == 0){
-                array_push($array, ucfirst($reverse[$i]));
-                
-            } else {
-                // for other i, always push in new array
-                array_push($array, $reverse[$i]);
-            }
-        }
-        // finaly use implode to get string with values of new array and return result of it
-        return implode(' ', $array); 
+        //$reverse = array_reverse($expl);
+        // Premiere lettre du premier mot en majuscule
+        //$reverse[0] = ucfirst($reverse[0]);
+        // finaly use implode to get string with values of reversed array and return result of it
+        //implode(' ', $reverse); 
+        
+        return ucfirst(implode(' ', array_reverse(explode(' ', $str))));
     }
 }
