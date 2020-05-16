0b7188913b2cb35d517325b06a9dcf0855d90372
Paracelse-ITOUA <61749869+Paracelse-ITOUA@users.noreply.github.com>
2020-05-16 06:11:01 +0200

    algo push

--- src/Service/Yodalizer.php
+++ src/Service/Yodalizer.php
@@ -10,7 +10,15 @@ class Yodalizer
 
     public static function yodalizeIt(string $str):string
     {
+        $explode = explode(' ', $str);
+        $reverse = array_reverse($explode);
+        $implode = implode(' ', $reverse);
+        $yodaWords = ucfirst($implode);
+   return $yodaWords;
         //TODO Write your code here,
+        
+        echo ucfirst();
+        echo ucwords();
         //TODO And return what we are waiting for at the end...
     }
 }
